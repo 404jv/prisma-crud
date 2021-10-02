507d40b5af0d8f5447bfe4d01e25c8ce53dec5cb
João Victor <joaovictorramalho7@gmail.com>
2021-10-02 14:54:43 -0300

    Create migrations

---
+++ prisma/migrations/20211002174626_create_todos/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "deadline" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
