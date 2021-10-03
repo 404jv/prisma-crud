3fd315ae68717faddb221e4b5967e574647c3501
João Victor <joaovictorramalho7@gmail.com>
2021-10-03 02:04:18 -0300

    Create show method

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -5,16 +5,19 @@ import { v4 as uuidV4 } from "uuid";
 interface IRequest {
   name: string;
   email: string;
+  username: string;
 }
 
 export default class UserController {
-  async create(request: Request, response: Response) {
-    const { name, email, username } = request.body;
+  async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, username }: IRequest = request.body;
 
     const prisma = new PrismaClient();
 
-    const userAlreadyExists = await prisma.user.findUnique({
-      where: { email }
+    const userAlreadyExists = await prisma.user.findFirst({
+      where: {
+        username
+      }
     });
 
     if (userAlreadyExists) {
@@ -29,22 +32,21 @@ export default class UserController {
         name,
         email,
         username
-      }
+      },
     });
 
     return response.status(201).json(user);
   }
 
-  async show(request: Request, response: Response) {
+  async show(request: Request, response: Response): Promise<Response> {
     const prisma = new PrismaClient();
 
-    const users = await prisma.user.findMany();
+    const users = await prisma.user.findMany({
+      include: {
+        todos: true
+      }
+    });
 
     return response.json(users);
-  }
-
-  async update(request: Request, response: Response) {
-    const { name, email }: IRequest = request.body;
-
   }
 }
