18765ba346de6e8fb6df1f2d1b2be6ca190e3fd0
João Victor <joaovictorramalho7@gmail.com>
2021-10-03 02:05:02 -0300

    Create routes to controllers

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import express from "express";
+import TodoController from "./controllers/TodoController";
 import UserController from "./controllers/UserController";
+import checksExistsUserAccount from "./middleware/checksExistsUserAccount";
 
 const app = express();
 app.use(express.json());
 
 const userController = new UserController();
+const todoController = new TodoController();
 
 app.post('/user', userController.create);
 app.get('/user', userController.show);
+
+app.post('/todo', checksExistsUserAccount, todoController.create);
 
 app.listen(3333, () => console.log("🚀 Server is running at: http://localhost:3333"));
